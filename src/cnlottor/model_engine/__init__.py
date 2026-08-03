47a5ede09c38c646f247b132b819593e447d33a1
b8vipvip <917931227@qq.com>
2026-08-03 12:08:23 +0800

    Record v0.3 model engine verification marker

--- src/cnlottor/model_engine/__init__.py
+++ src/cnlottor/model_engine/__init__.py
@@ -1,10 +1,28 @@
+"""Public model-engine API for the CNlottor v0.3 release."""
+
 from .contracts import LotteryPredictor, ModelTrainer
-from .dataset import EncodedDraw, EncodedPool, SupervisedWindow, build_supervised_windows, encode_draw, encode_pool
+from .dataset import (
+    EncodedDraw,
+    EncodedPool,
+    SupervisedWindow,
+    build_supervised_windows,
+    encode_draw,
+    encode_pool,
+)
 from .service import TorchModelService, TrainConfig, TrainReport
 from .torch_backend import TorchUnavailableError
 
 __all__ = [
-    "LotteryPredictor", "ModelTrainer", "EncodedDraw", "EncodedPool", "SupervisedWindow",
-    "build_supervised_windows", "encode_draw", "encode_pool", "TorchModelService",
-    "TrainConfig", "TrainReport", "TorchUnavailableError",
+    "LotteryPredictor",
+    "ModelTrainer",
+    "EncodedDraw",
+    "EncodedPool",
+    "SupervisedWindow",
+    "build_supervised_windows",
+    "encode_draw",
+    "encode_pool",
+    "TorchModelService",
+    "TrainConfig",
+    "TrainReport",
+    "TorchUnavailableError",
 ]
