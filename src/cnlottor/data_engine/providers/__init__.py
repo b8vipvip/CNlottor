908ee23319716da669ce25cd1fca739a5b82dc70
b8vipvip <917931227@qq.com>
2026-08-06 19:31:33 +0800

    Export fallback and KL8 text providers

--- src/cnlottor/data_engine/providers/__init__.py
+++ src/cnlottor/data_engine/providers/__init__.py
@@ -1,14 +1,21 @@
 from .base import LotteryProvider, RawDraw
-from .composite import CompositeLotteryProvider, build_default_provider
+from .composite import (
+    CompositeLotteryProvider,
+    FallbackLotteryProvider,
+    build_default_provider,
+)
 from .cwl_official import ChinaWelfareLotteryProvider
 from .datachart500 import DataChart500Provider, HttpSettings
+from .text917500 import Text917500Provider
 
 __all__ = [
     "ChinaWelfareLotteryProvider",
     "CompositeLotteryProvider",
     "DataChart500Provider",
+    "FallbackLotteryProvider",
     "HttpSettings",
     "LotteryProvider",
     "RawDraw",
+    "Text917500Provider",
     "build_default_provider",
 ]
