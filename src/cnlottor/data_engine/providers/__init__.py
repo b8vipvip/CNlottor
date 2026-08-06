06dfe9df6e94eea678247a8f11abe9ce9ac537c8
b8vipvip <917931227@qq.com>
2026-08-06 19:17:34 +0800

    Export official and composite providers

--- src/cnlottor/data_engine/providers/__init__.py
+++ src/cnlottor/data_engine/providers/__init__.py
@@ -1,4 +1,14 @@
 from .base import LotteryProvider, RawDraw
+from .composite import CompositeLotteryProvider, build_default_provider
+from .cwl_official import ChinaWelfareLotteryProvider
 from .datachart500 import DataChart500Provider, HttpSettings
 
-__all__ = ["DataChart500Provider", "HttpSettings", "LotteryProvider", "RawDraw"]
+__all__ = [
+    "ChinaWelfareLotteryProvider",
+    "CompositeLotteryProvider",
+    "DataChart500Provider",
+    "HttpSettings",
+    "LotteryProvider",
+    "RawDraw",
+    "build_default_provider",
+]
