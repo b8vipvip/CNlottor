d4350fbd700b3298dfacc885e4ca7f734c2f8aad
b8vipvip <917931227@qq.com>
2026-08-06 19:42:20 +0800

    Export KL8 trend provider

--- src/cnlottor/data_engine/providers/__init__.py
+++ src/cnlottor/data_engine/providers/__init__.py
@@ -6,12 +6,14 @@ from .composite import (
 )
 from .cwl_official import ChinaWelfareLotteryProvider
 from .datachart500 import DataChart500Provider, HttpSettings
+from .datachart_kl8 import DataChartKl8TrendProvider
 from .text917500 import Text917500Provider
 
 __all__ = [
     "ChinaWelfareLotteryProvider",
     "CompositeLotteryProvider",
     "DataChart500Provider",
+    "DataChartKl8TrendProvider",
     "FallbackLotteryProvider",
     "HttpSettings",
     "LotteryProvider",
